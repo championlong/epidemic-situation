7b1d7a9bc05cffd55db633f01a6f118990468dcf
championlong <championlong@users.noreply.github.com>
2020-09-14 16:02:26 +0800

    test
    test describe

---
+++ blog-app/src/components/map_index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="demo"></div>
+</template>
+
+<script>
+  // import BMap from 'BMap'
+
+  export default {
+    name: 'Map',
+
+    data() {
+      return {
+
+      }
+    },
+    mounted () {
+      this.baiduMap()
+    },
+    methods: {
+      baiduMap () {
+        var map = new BMap.Map('allmap')
+        var point = new BMap.Point(111.742579, 40.818675)
+        map.centerAndZoom(point, 12)
+        var marker = new BMap.Marker(point) // 创建标注
+        map.addOverlay(marker) // 将标注添加到地图中
+      }
+    }
+  }
+</script>
+
+<style>
+  .demo {
+    width: 200px;
+    height: 200px;
+    background-color: black;
+  }
+</style>
